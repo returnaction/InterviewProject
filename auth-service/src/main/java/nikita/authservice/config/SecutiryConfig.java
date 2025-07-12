ac525668ba5ad1ca0f3b3a33ad793d9de664405d
returnaction <obergannv@gmail.com>
2025-07-13 02:07:22 +0300

    перенесли все в config-server цщworks

--- auth-service/src/main/java/nikita/authservice/config/SecutiryConfig.java
+++ auth-service/src/main/java/nikita/authservice/config/SecutiryConfig.java
@@ -2,7 +2,10 @@ package nikita.authservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,10 +19,21 @@ public class SecutiryConfig {
     }
 
     // TODO Добавить цепочки после того как узнаем все наши пути
-    @Bean // Sozdaem tsepochku filtrov bezopasnosti
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll()) // Razreshaem vse zaprosy bez autentifikatsii
-                .csrf(csrf -> csrf.disable()); // Otklyuchaem zashchitu ot CSRF (dlya prostoty na etape razrabotki)
-        return http.build(); // Vozvrashchaem nastroennuyu tsepochku
+        return http
+                .securityMatcher("/**")
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(csrf -> csrf.disable())
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
